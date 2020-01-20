faea8a43b92ccc23cc9688011c549b9df525274d
udanzo-p <59428879+udanzo-p@users.noreply.github.com>
2020-01-20 09:42:17 +0530

    Add files via upload

---
+++ oddeve.py
@@ -0,0 +1,6 @@
+#Odd Even Program
+num=int(input("Enter no. : "))
+if(num%2==0) :
+ print("The given no. is even")
+else:
+ print("No. is odd")
---
+++ sumusingfunbyme.py
@@ -0,0 +1,8 @@
+def sumnumfun(x,y):
+   sum = x + y
+   return sum
+print('Enter no.')
+q = int(input(":"))
+w = int(input(":"))
+
+print("The sum is", sumnumfun(q, w))
